4ba3d6009e5798b02cbd4dfc1d2721120341e795
Levi-coder07 <82822546+Levi-coder07@users.noreply.github.com>
2021-08-01 20:32:01 -0500

    Update MainMenu.h

--- Save-the-galaxy/MainMenu.h
+++ Save-the-galaxy/MainMenu.h
@@ -1,15 +1,13 @@
-#pragma once
-#include "SFML/Graphics.hpp"
 #include <iostream>
 using namespace std;
 using namespace sf;
 
-#define Max_main_menu 4
+#define Max_main_menu 5
 class MainMenu
 {
 public:
 	MainMenu(float width, float height);
-
+	
 	void draw(RenderWindow& window);
 	void MoveUp();
 	void MoveDown();
